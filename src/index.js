af28f88dd67af5ce020e2969180c64de024fae19
Pablo Jacobo Viña Rebolledo <pablojvr@ext.zara.com>
2022-07-10 03:08:55 +0200

    [AFTF-1] feat(NavBar): add navBar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { UserProvider } from "./providers/UserProvider";
+import { AftRouter } from "./routers/AftRouter";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const divRoot = document.getElementById("root");
+const root = createRoot(divRoot);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <UserProvider>
+    <AftRouter />
+  </UserProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/login/LoginScreen.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LoginScreen = () => {
+  return (
+    <>
+      <h1>AFT LOGIN</h1>
+    </>
+  );
+};
